22a040e1945a759866885592eba0b07c930c74e6
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2017-12-11 07:02:26 +0100

    feat(transpiler): add Angular AST transform support

--- src/backend/transpiler.ts
+++ src/backend/transpiler.ts
@@ -1,17 +1,26 @@
 import * as ts from 'typescript'
 
 import {TranspileResult} from '../interfaces'
+import {replaceResources} from './angular'
 
 export class Transpiler {
 	private readonly compilerOptions: ts.CompilerOptions
+	private readonly transformers = [replaceResources(() => true)]
 
 	constructor(options: ts.ParsedCommandLine) {
 		this.compilerOptions = options.options
 	}
 
 	public transpile(code: string, fileName: string): TranspileResult {
-		const {compilerOptions} = this
-		const {outputText: js, sourceMapText: sourceMap} = ts.transpileModule(code, {compilerOptions, fileName})
+		const {compilerOptions, transformers: before} = this
+		const {outputText: js, sourceMapText: sourceMap} = ts.transpileModule(code, {
+			compilerOptions, fileName,
+			transformers: {before}
+		})
+
+		if(/app.component/.test(fileName)) {
+			console.log('\n\nResult %s\n\n', js)
+		}
 
 		return {
 			sources: {js, sourceMap}
